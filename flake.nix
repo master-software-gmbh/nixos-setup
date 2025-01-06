a6e42aae5c92935b0f2ea1d6c949757d5b567949
Marian Eickmeyer <mail@marianheinsen.com>
2025-01-06 20:14:47 +0100

    refactor: use system module from utilities

--- flake.nix
+++ flake.nix
@@ -1,14 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11-small";
+    utilities.url = "github:master-software-gmbh/nixos-utilities";
   };
 
-  outputs = { nixpkgs }: {
+  outputs = { self, nixpkgs, utilities }: {
     nixosConfigurations = {
       default = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./configuration.nix
+          utilities.nixosModules.system
+          ({ config, pkgs, ... }: {
+            modules.system = {
+              sshPort = 41298;
+              hostName = "nixos";
+              stateVersion = "24.05";
+              sshAuthorizedKeys = [
+                "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNENHqsxWyb9JOsuK+Tj3eELtIUu17Mb1tSb6urTC88"
+              ];
+            };
+          })
         ];
       };
     };
